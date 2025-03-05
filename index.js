94819772e3a4380ce379ca4accdf7c04ae798c3b
hourfrozen <hourfrozen4@gmail.com>
2025-03-05 15:37:00 -0500

    make fucking rad spinnin cube

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+// initial
+
+// objects
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+// renderer
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // rend
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
+
+// rn its a spinning cube, omg can you believe it
